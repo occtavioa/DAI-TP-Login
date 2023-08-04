44c4c42f7442d9fcac1501f9a5b68c8ba60d37c0
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-04 12:03:13 -0300

    primer commit

---
+++ api/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 5000
+const bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post("/login", (req, res) => {
+  let usuario = req.body;
+
+  if(usuario.nombre === "nombre" && usuario.contrasena === "contraseña") {
+      res.send("bien")
+  } else {
+      res.send("mal")
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
